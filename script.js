c05856ea67b5003210c9c0e4d917fa70c6666512
adikrovi <jrkrovi@gmail.com>
2025-05-06 18:04:50 -0400

    updated dark theme

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark' || !savedTheme) {
+  document.body.classList.add('dark-mode');
+}
+
 const toggleButton = document.getElementById('toggle-theme');
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
+  const currentTheme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+  localStorage.setItem('theme', currentTheme);
 });
